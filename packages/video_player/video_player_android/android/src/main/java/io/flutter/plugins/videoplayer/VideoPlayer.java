4203c45a4d7454efee2d0179cf8a3afbc544ddfd
atsumi <atsumi@mail.bizen.jp>
2025-09-02 14:52:16 +0900

    update android projects

--- packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -20,100 +20,107 @@ import androidx.media3.exoplayer.ExoPlayer;
  * <p>It provides methods to control playback, adjust volume, and handle seeking.
  */
 public abstract class VideoPlayer {
-  @NonNull private final ExoPlayerProvider exoPlayerProvider;
-  @NonNull private final MediaItem mediaItem;
-  @NonNull private final VideoPlayerOptions options;
-  @NonNull protected final VideoPlayerCallbacks videoPlayerEvents;
-  @NonNull protected ExoPlayer exoPlayer;
-
-  /** A closure-compatible signature since {@link java.util.function.Supplier} is API level 24. */
-  public interface ExoPlayerProvider {
-    /**
-     * Returns a new {@link ExoPlayer}.
-     *
-     * @return new instance.
-     */
     @NonNull
-    ExoPlayer get();
-  }
+    private final ExoPlayerProvider exoPlayerProvider;
+    @NonNull
+    private final MediaItem mediaItem;
+    @NonNull
+    private final VideoPlayerOptions options;
+    @NonNull
+    protected final VideoPlayerCallbacks videoPlayerEvents;
+    @NonNull
+    protected ExoPlayer exoPlayer;
 
-  public VideoPlayer(
-      @NonNull VideoPlayerCallbacks events,
-      @NonNull MediaItem mediaItem,
-      @NonNull VideoPlayerOptions options,
-      @NonNull ExoPlayerProvider exoPlayerProvider) {
-    this.videoPlayerEvents = events;
-    this.mediaItem = mediaItem;
-    this.options = options;
-    this.exoPlayerProvider = exoPlayerProvider;
-    this.exoPlayer = createVideoPlayer();
-  }
+    /**
+     * A closure-compatible signature since {@link java.util.function.Supplier} is API level 24.
+     */
+    public interface ExoPlayerProvider {
+        /**
+         * Returns a new {@link ExoPlayer}.
+         *
+         * @return new instance.
+         */
+        @NonNull
+        ExoPlayer get();
+    }
 
-  @NonNull
-  protected ExoPlayer createVideoPlayer() {
-    ExoPlayer exoPlayer = exoPlayerProvider.get();
-    exoPlayer.setMediaItem(mediaItem);
-    exoPlayer.prepare();
+    public VideoPlayer(
+            @NonNull VideoPlayerCallbacks events,
+            @NonNull MediaItem mediaItem,
+            @NonNull VideoPlayerOptions options,
+            @NonNull ExoPlayerProvider exoPlayerProvider) {
+        this.videoPlayerEvents = events;
+        this.mediaItem = mediaItem;
+        this.options = options;
+        this.exoPlayerProvider = exoPlayerProvider;
+        this.exoPlayer = createVideoPlayer();
+    }
 
-    exoPlayer.addListener(createExoPlayerEventListener(exoPlayer));
-    setAudioAttributes(exoPlayer, options.mixWithOthers);
+    @NonNull
+    protected ExoPlayer createVideoPlayer() {
+        ExoPlayer exoPlayer = exoPlayerProvider.get();
+        exoPlayer.setMediaItem(mediaItem);
+        exoPlayer.prepare();
 
-    return exoPlayer;
-  }
+        exoPlayer.addListener(createExoPlayerEventListener(exoPlayer));
+        setAudioAttributes(exoPlayer, options.mixWithOthers);
 
-  @NonNull
-  protected abstract ExoPlayerEventListener createExoPlayerEventListener(
-      @NonNull ExoPlayer exoPlayer);
+        return exoPlayer;
+    }
 
-  void sendBufferingUpdate() {
-    videoPlayerEvents.onBufferingUpdate(exoPlayer.getBufferedPosition());
-  }
+    @NonNull
+    protected abstract ExoPlayerEventListener createExoPlayerEventListener(
+            @NonNull ExoPlayer exoPlayer);
 
-  private static void setAudioAttributes(ExoPlayer exoPlayer, boolean isMixMode) {
-    exoPlayer.setAudioAttributes(
-        new AudioAttributes.Builder().setContentType(C.AUDIO_CONTENT_TYPE_MOVIE).build(),
-        !isMixMode);
-  }
+    void sendBufferingUpdate() {
+        videoPlayerEvents.onBufferingUpdate(exoPlayer.getBufferedPosition());
+    }
 
-  void play() {
-    exoPlayer.play();
-  }
+    private static void setAudioAttributes(ExoPlayer exoPlayer, boolean isMixMode) {
+        exoPlayer.setAudioAttributes(
+                new AudioAttributes.Builder().setContentType(C.AUDIO_CONTENT_TYPE_MOVIE).build(),
+                !isMixMode);
+    }
 
-  void pause() {
-    exoPlayer.pause();
-  }
+    void play() {
+        exoPlayer.play();
+    }
 
-  void setLooping(boolean value) {
-    exoPlayer.setRepeatMode(value ? REPEAT_MODE_ALL : REPEAT_MODE_OFF);
-  }
+    void pause() {
+        exoPlayer.pause();
+    }
 
-  void setVolume(double value) {
-    float bracketedValue = (float) Math.max(0.0, Math.min(1.0, value));
-    exoPlayer.setVolume(bracketedValue);
-  }
+    void setLooping(boolean value) {
+        exoPlayer.setRepeatMode(value ? REPEAT_MODE_ALL : REPEAT_MODE_OFF);
+    }
 
-  void setPlaybackSpeed(double value) {
-    // We do not need to consider pitch and skipSilence for now as we do not handle them and
-    // therefore never diverge from the default values.
-    final PlaybackParameters playbackParameters = new PlaybackParameters(((float) value));
+    void setVolume(double value) {
+        float bracketedValue = (float) Math.max(0.0, Math.min(1.0, value));
+        exoPlayer.setVolume(bracketedValue);
+    }
 
-    exoPlayer.setPlaybackParameters(playbackParameters);
-  }
+    void setPlaybackSpeed(double value) {
+        // We do not need to consider pitch and skipSilence for now as we do not handle them and
+        // therefore never diverge from the default values.
+        final PlaybackParameters playbackParameters = new PlaybackParameters(((float) value));
 
-  void seekTo(int location) {
-    exoPlayer.seekTo(location);
-  }
+        exoPlayer.setPlaybackParameters(playbackParameters);
+    }
 
-  long getPosition() {
-    return exoPlayer.getCurrentPosition();
-  }
+    void seekTo(int location) {
+        exoPlayer.seekTo(location);
+    }
 
-  @NonNull
-  public ExoPlayer getExoPlayer() {
-    return exoPlayer;
-  }
+    long getPosition() {
+        return exoPlayer.getCurrentPosition();
+    }
 
-  public void dispose() {
-    exoPlayer.release();
-  }
+    @NonNull
+    public ExoPlayer getExoPlayer() {
+        return exoPlayer;
+    }
+
+    public void dispose() {
+        exoPlayer.release();
+    }
 }
